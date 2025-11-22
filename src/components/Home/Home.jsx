7a94e5eb8bebd01d7a289bba32808fb21e1deba9
Priyanshu Dubey <129979141+Priyanshu1119@users.noreply.github.com>
2025-11-22 22:14:54 +0530

    Change welcome message in Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
          
         <div className="mx-auto w-full max-w-7xl">
-                <h1 className='text-center text-5xl font-bold text-gray-500  underline underline-offset-4'>WELCOME To My Portfolio</h1>
+                <h1 className='text-center text-5xl font-bold text-gray-500  underline underline-offset-4'>WELCOME To My World</h1>
             <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
                 <div className="relative z-10 max-w screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
